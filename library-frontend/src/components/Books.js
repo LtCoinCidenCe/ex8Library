4f08687aba5ab8e12a42232a5a7bc49c4e1224fa
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-11-21 23:42:54 +0200

    8.9

--- library-frontend/src/components/Books.js
+++ library-frontend/src/components/Books.js
@@ -1,26 +1,22 @@
+import { useQuery } from '@apollo/client'
 import React from 'react'
+import { ALL_BOOKS } from '../queries'
 
 const Books = (props) => {
-  if (!props.show) {
+  const bookResults = useQuery(ALL_BOOKS)
+  if (!props.show)
     return null
-  }
-
-  const books = []
+  if (bookResults.loading)
+    return <div>loading...</div>
+  const books = bookResults.data.allBooks
 
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
-            <th></th>
-            <th>
-              author
-            </th>
-            <th>
-              published
-            </th>
+            <th></th><th>author</th><th>published</th>
           </tr>
           {books.map(a =>
             <tr key={a.title}>
