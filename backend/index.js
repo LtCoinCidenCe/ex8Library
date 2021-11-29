34b037249498eb822b884d07f4c281a99bff5ca7
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-11-29 16:16:18 +0200

    8.16

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,10 @@
-const { ApolloServer, gql, UserInputError } = require('apollo-server')
+const { ApolloServer, gql, UserInputError, AuthenticationError } = require('apollo-server')
 const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 require('dotenv').config()
-const { v1: uuid } = require('uuid')
 const Author = require('./models/Author')
 const Book = require('./models/Book')
+const User = require('./models/User')
 
 const MONGODB_URI = process.env.MONGODB_URI
 console.log('connecting to', MONGODB_URI)
@@ -14,7 +15,6 @@ mongoose.connect(MONGODB_URI)
   .catch((error) => {
     console.log('error connection to MongoDB:', error.message)
   })
-
 
 let authors = [
   {
@@ -99,6 +99,7 @@ const typeDefs = gql`
     authorCount: Int!
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
+    me: User
   }
   
   type Mutation {
@@ -118,6 +119,16 @@ const typeDefs = gql`
       name: String!
       setBornTo: Int!
     ):Author
+    
+    createUser(
+      username: String!
+      favoriteGenre: String!
+    ): User
+    
+    login(
+      username: String!
+      password: String!
+    ): Token
   }
   
   type Book {
@@ -133,6 +144,14 @@ const typeDefs = gql`
     id: String!
     born: Int
   }
+  type User {
+    username: String!
+    favoriteGenre: String!
+    id: ID!
+  }
+  type Token {
+    value: String!
+  }
 `
 // It seems like mongoose-unique-validator is not required in mongoose version 6 anymore
 // E11000 duplicate key error collection gets caught
@@ -144,10 +163,14 @@ const resolvers = {
     allBooks: async (root, args) => {
       return await Book.find({}).populate('author')
     },
-    allAuthors: async () => await Author.find({})
+    allAuthors: async () => await Author.find({}),
+    me: (root, args, context) => context.currentUser
   },
   Mutation: {
-    addBook: async (root, args) => {
+    addBook: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
       const newBook = new Book({
         title: args.title,
         published: args.published,
@@ -179,9 +202,36 @@ const resolvers = {
       return newEntry
     },
 
-    editAuthor: async (root, args) => {
+    editAuthor: async (root, args, context) => {
+      if (!context.currentUser) {
+        throw new AuthenticationError("not authenticated")
+      }
       const au = await Author.findOneAndUpdate({ name: args.name }, { born: args.setBornTo }, { new: true })
       return au
+    },
+
+    createUser: async (root, args) => {
+      const user = new User({ username: args.username, favoriteGenre: args.favoriteGenre })
+      try {
+        await user.save()
+      }
+      catch (error) {
+        throw new UserInputError(error.message, { invalidArgs: args })
+      }
+      return user
+    },
+
+    login: async (root, args) => {
+      const user = await User.findOne({ username: args.username })
+      // hardcoded password 'secret'
+      if (!user || args.password !== 'secret') {
+        throw new UserInputError("wrong credentials")
+      }
+      const userForToken = {
+        username: user.username,
+        id: user._id,
+      }
+      return { value: jwt.sign(userForToken, process.env.JWT_SECRET) }
     }
   },
   Author: {
@@ -193,6 +243,16 @@ const resolvers = {
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  context: async ({ req }) => {
+    const auth = req ? req.headers.authorization : null
+    if (auth && auth.toLowerCase().startsWith('bearer ')) {
+      const decodedToken = jwt.verify(
+        auth.substring(7), process.env.JWT_SECRET
+      )
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  }
 })
 
 server.listen().then(({ url }) => {
